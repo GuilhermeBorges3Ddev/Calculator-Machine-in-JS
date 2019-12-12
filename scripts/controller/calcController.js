78c8720256fcb75d0b6705dcfe60979d052714ac
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-12-11 23:15:08 -0300

    Manipulating the DOM with JS functions in document object

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -5,7 +5,17 @@ class CalcController {
         //Notation with "_" in attributes refers private attributes
         this._displayCalc = "0";
         this._currentDate;
+        this.initialize();
+    }
 
+    initialize(){
+       let displayCalcEl = document.querySelector("#display");
+       let dataEl = document.querySelector("#data");
+       let timeEl = document.querySelector("#hora");
+
+       displayCalcEl.innerHTML = "081194";
+       dataEl.innerHTML = "11/12/2019";
+       timeEl.innerHTML = "23:30"
     }
 
     get displayCalc(){
