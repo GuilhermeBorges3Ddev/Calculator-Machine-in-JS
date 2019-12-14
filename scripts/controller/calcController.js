a2969a0212bd99059ee55c9243c7e569d5982650
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-12-14 01:23:04 -0300

    Date and hour manipulated with setter and getter

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -5,15 +5,15 @@ class CalcController {
         //Notation with "_" in attributes refers private attributes, works only inside the class
         this._currentDate;
         this._displayCalcEl = document.querySelector("#display");
-        this._dataEl = document.querySelector("#data");
+        this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this.initialize();
     }
 
     initialize(){
-       this._displayCalcEl.innerHTML = "081194";
-       this._dataEl.innerHTML = "11/12/2019";
-       this._timeEl.innerHTML = "23:30"
+       this._displayCalcEl.innerHTML = "000000";
+       this._dateEl.innerHTML = "13/12/2019";
+       this._timeEl.innerHTML = "23:59"
     }
 
     get displayCalc(){
@@ -25,11 +25,25 @@ class CalcController {
     }
 
     get currentDate(){
-        return this._currentDate;
+        return new Date();
     }
 
+    //Class 'Date' has interesting methods, like: toLocaleDateString('pt-BR', {month: 'long'}), getFullYear(), getMonth() and more...
     set currentDate(value){
         this._currentDate = value;
     }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
+    }
+
 
 }
